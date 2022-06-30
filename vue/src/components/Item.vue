ecd0163083ba732f4a7eb4323e69065eeb092643
Leo Ribas <leonas.ribas@gmail.com>
2022-06-30 10:41:29 -0300

    ✨ Creates Item component

---
+++ vue/src/components/Item.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+    props: ["code", "name"],
+};
+</script>
+
+<template>
+    <li class="p-8 rounded-md">
+        <p class="font-sans font-bold text-lg mb-2">{{ code }}</p>
+        <p class="font-sans text-base text-slate-500">{{ name }}</p>
+    </li>
+</template>
